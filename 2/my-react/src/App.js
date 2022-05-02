bc3bba3dcad8e2abb19ce85351c30a6c621fe986
Tatiana-Dm <83395904+Tatiana-Dm@users.noreply.github.com>
2022-05-02 09:36:35 +0300

    Add files via upload

---
+++ 2/my-react/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <div>Logo</div>
+        <div className="App-header-item">
+          <div className="App-header-list">Main</div>
+          <div className="App-header-list">Contacts</div>
+          <div className="App-header-list">About</div>
+        </div>
+      </header>
+
+      <main className="App-main">
+        <h1 className="App-text">This is header title</h1>
+        <p className="App-text"> Lorem ipsum dolor sit amet, qui amet facilisi antiopam at, equidem labores delectus mea ea, stet delenit suavitate te est. Pro ex omnesque dissentiet. Verterem vituperata his ut. Suas referrentur cu sed, audire oblique vel eu. Ne idque nihil corpora per, ad quo adipisci gloriatur. Est liber doctus similique ad, duo oporteat partiendo appellantur ea. Eu duis reque erant est.</p>
+        <p className="App-text">Lorem ipsum dolor sit amet, qui amet facilisi antiopam at, equidem labores delectus mea ea, stet delenit suavitate te est. Pro ex omnesque dissentiet. Verterem vituperata his ut. Suas referrentur cu sed, audire oblique vel eu. Ne idque nihil corpora per, ad quo adipisci gloriatur. Est liber doctus similique ad, duo oporteat partiendo appellantur ea. Eu duis reque erant est.</p>
+        <p className="App-text">Lorem ipsum dolor sit amet, qui amet facilisi antiopam at, equidem labores delectus mea ea, stet delenit suavitate te est. Pro ex omnesque dissentiet. Verterem vituperata his ut. Suas referrentur cu sed, audire oblique vel eu. Ne idque nihil corpora per, ad quo adipisci gloriatur. Est liber doctus similique ad, duo oporteat partiendo appellantur ea. Eu duis reque erant est.</p>
+        <p className="App-text">Lorem ipsum dolor sit amet, qui amet facilisi antiopam at, equidem labores delectus mea ea, stet delenit suavitate te est. Pro ex omnesque dissentiet. Verterem vituperata his ut. Suas referrentur cu sed, audire oblique vel eu. Ne idque nihil corpora per, ad quo adipisci gloriatur. Est liber doctus similique ad, duo oporteat partiendo appellantur ea. Eu duis reque erant est.</p>
+      </main>
+
+      <footer className="App-footer">
+        <p className="App-text">React</p>
+        <p className="App-text">Mozilla Developer Network</p>
+        <p className="App-text">My GitHub Profile</p>
+        <p className="App-text-end">2022, Odesa, Ukraine</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
